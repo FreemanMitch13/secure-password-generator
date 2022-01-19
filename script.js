c18cb7571ace64eb2460bf668832d9969947c921
Mitchell Freeman <mitchell.w.freeman13@gmail.com>
2022-01-19 16:17:03 -0500

    updating html and js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength = document.querySelector("#length");
-var passwordUpper = document.querySelector("#uppercase");
-var passwordLower = document.querySelector("#lowercase");
-var passwordNumeric = document.querySelector("#numeric");
-var passwordSpecial = document.querySelector("#special");
+var passwordUpper = document.querySelector("upperCheck");
+var passwordNumeric = document.querySelector("numericCheck");
+var passwordSpecial = document.querySelector("specialCheck");
 var form = document.querySelector("form");
 
 // Write password to the #password input
@@ -18,17 +16,33 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
 
 //create a function called generate password, function returns back a string as a password
 
-function generatePassword() {
-    var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = passwordLength.value;
+function generatePassword() {    
+    var characters = "abcdefghijklmnopqrstuvwxyz";
+    var numCharacters = "0123456789";
+    var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var specialCharacters = "!@#$%^&*()";
+    
+    var includeNumbers = document.getElementById("numericCheck").checked;
+    var includeUppers = document.getElementById("uppercaseCheck").checked;
+    var includeSpecial = document.getElementById("specialCheck").checked;
+
+    includeNumbers && (characters += numCharacters);
+    includeUppers && (characters += upperCharacters);
+    includeSpecial && (characters += specialCharacters);
+
     var password = "";
+    var passwordLength = document.getElementById("lengthValue").value;
+
     for (var i =0; i <= passwordLength; i++) {
         var randomCharacter = Math.floor(Math.random() * characters.length);
         password += characters.substring(randomCharacter, randomCharacter +1);
     }
+    
     document.getElementById("password").value = password;
+    
 }
